55b5ba256cfd4f4c8732fcb78ba863c009dcf8a0
p30arena <p30arena@gmail.com>
2016-03-25 00:00:05 +0430

    find the main interface connected to the internet

--- Open.Nat/Utils/IPAddressesProvider.cs
+++ Open.Nat/Utils/IPAddressesProvider.cs
@@ -30,11 +30,15 @@ using System.Linq;
 using System.Net;
 using System.Net.NetworkInformation;
 using System.Net.Sockets;
+using System.Runtime.InteropServices;
 
 namespace Open.Nat
 {
     internal class IPAddressesProvider : IIPAddressesProvider
     {
+        [DllImport("iphlpapi.dll", SetLastError = true)]
+        static extern int GetBestInterface(UInt32 DestAddr, out UInt32 BestIfIndex);
+
         #region IIPAddressesProvider Members
 
         public IEnumerable<IPAddress> UnicastAddresses()
@@ -56,12 +60,25 @@ namespace Open.Nat
 
         private static IEnumerable<IPAddress> IPAddresses(Func<IPInterfaceProperties, IEnumerable<IPAddress>> ipExtractor)
         {
+            // Google's IP Address
+            UInt32 ipv4AddressAsUInt32 = BitConverter.ToUInt32(IPAddress.Parse("173.194.122.200").GetAddressBytes(), 0);
+            UInt32 interfaceindex;
+
+            int result = GetBestInterface(ipv4AddressAsUInt32, out interfaceindex);
+
+            if (result != 0)
+            {
+                return Enumerable.Empty<IPAddress>();
+            }
+
             return from networkInterface in NetworkInterface.GetAllNetworkInterfaces()
                    where
                        networkInterface.OperationalStatus == OperationalStatus.Up ||
                        networkInterface.OperationalStatus == OperationalStatus.Unknown
                    let properties = networkInterface.GetIPProperties()
+                   let ipv4Properties = properties.GetIPv4Properties()
                    from address in ipExtractor(properties)
+                   where ipv4Properties != null && ipv4Properties.Index == interfaceindex
                    where address.AddressFamily == AddressFamily.InterNetwork
                    select address;
         }
